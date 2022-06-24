9ef47225516ebe9528cbdbe7db84a28cba38372e
ycoodr <ycoodr@gmail.com>
2022-06-24 12:45:05 +0300

    все

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 # Create your views here.
 
@@ -9,4 +10,11 @@ def index(request):
 
 def search(request):
     city = request.GET['city']
-    return HttpResponse(city)
+    image_url = "http://openweathermap.org/img/wn/"
+    url = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=metric&appid=934adaa7fb59ca72db3ef92c70582552"
+    response = requests.get(url)
+    jsonResponse = response.json()
+    description = jsonResponse['weather'][0]['description']
+    icon_code = jsonResponse['weather'][0]['icon']
+    image_icon = image_url + icon_code + '.png'
+    return render(request, 'blog/index.html', {'jsonResponse': jsonResponse, 'description': description, 'image_icon': image_icon})
